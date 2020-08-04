d5847a0918f1f9469256acaebf98919c519ab488
Matt Williamson <matt@mattwilliamson.us>
2020-08-04 09:22:08 -0400

    Fix acer edid for Ubuntu 20.04 and radeon card

---
+++ acer-edid-fix-radeon.sh
@@ -0,0 +1,2 @@
+xrandr --addmode DisplayPort-2 1920x1080
+xrandr --output DisplayPort-0 --primary --mode 2560x1600 --pos 1080x117 --rotate normal --output DisplayPort-1 --mode 1920x1080 --pos 3640x0 --rotate left --output DisplayPort-2 --mode 1920x1080 --pos 0x0 --rotate right --output HDMI-A-0 --off
